6805d6abcec1ea6d94d563b05738e6c90c2a14ff
Гавриш Олександр <sashawwe21@gmail.com>
2024-06-17 16:05:55 +0300

    main

--- syner/src/app/app.component.ts
+++ syner/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,30 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  currentSection: string = ``;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = document.querySelectorAll('section');
+    let current = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      if (window.pageYOffset >= sectionTop - 60) {
+        current = section.getAttribute('id')!;
+      }
+    });
+
+    this.currentSection = current;
+  }
+
+  scrollToSection(section: string) {
+    document.getElementById(section)!.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
 
