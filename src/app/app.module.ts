b03f3289d01f524a953985d1f83b67e4bf42da88
Гавриш Олександр <sashawwe21@gmail.com>
2024-07-09 15:18:40 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
-// app.module.ts
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterOutlet } from '@angular/router';
-import { SwiperModule } from 'ngx-swiper-wrapper';
 import { NgClass } from '@angular/common';
 import { MatterModule } from './matter/matter.module'; // Імпорт MatterModule
 
 @NgModule({
   declarations: [
-    AppComponent,
   ],
   imports: [
     BrowserModule,
     RouterOutlet,
-    SwiperModule,
     NgClass,
     MatterModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
