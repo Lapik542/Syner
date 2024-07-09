965d439b022267216867e4f593608d460d9b409a
Гавриш Олександр <sashawwe21@gmail.com>
2024-07-09 15:10:48 +0300

    main

---
+++ src/app/matter/matter.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatterComponent } from './matter.component';
+
+@NgModule({
+  declarations: [MatterComponent],
+  imports: [CommonModule],
+  exports: [MatterComponent]
+})
+export class MatterModule { }
+export { MatterComponent };
+
