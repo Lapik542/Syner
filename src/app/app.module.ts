875f267972b595802262f8bb4df01c8283a0577e
Гавриш Олександр <sashawwe21@gmail.com>
2024-07-15 14:21:27 +0300

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatterModule } from './matter/matter.module';
 import { AppComponent } from './app.component';
-import { SwiperModule } from 'swiper/angular/angular/src/public-api';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   imports: [
